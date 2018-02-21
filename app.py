46e6474c3f1326be6bc2c2e4b6c426d92cc0db18
Adam Becvar <becvar.adam666@gmail.com>
2018-02-21 16:15:05 +0100

    Add download function

---
+++ app.py
@@ -0,0 +1,19 @@
+import requests
+import shutil
+
+def download_file(url):
+    split = url.split("/")
+    file_name = split[len(split)-1]
+    print(file_name)
+    r = requests.get(url, stream=True)
+    if r.status_code == 200:
+        with open(file_name, 'wb') as f:
+            r.raw.decode_content = True
+            shutil.copyfileobj(r.raw, f)
+
+
+download_file("https://ucr.fbi.gov/crime-in-the-u.s/2013/crime-in-the-u.s.-2013/tables/1tabledatadecoverviewpdf/"
+              "table_1_crime_in_the_united_states_by_volume_and_rate_per_100000_inhabitants_1994-2013.xls/output.xls")
+
+
+

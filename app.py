82c9730ac420e331fce650641f6251bbdc5cc7a6
J-Egeberg <jregeberg@gmail.com>
2018-02-25 13:48:49 +0100

    added question 2

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ import shutil
 import openpyxl
 import xlrd
 from openpyxl.workbook import Workbook as openpyxlWorkbook
-
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
 
 def download_file(url):
     split = url.split("/")
@@ -42,3 +45,43 @@ def first_question():
 
 
 first_question()
+
+def average_murder_per_year(sheet, row_num):
+    return sheet.cell(row_num, 5).value
+def average_rape_per_year(sheet, row_num):
+    return sheet.cell(row_num, 7).value
+def average_robbery_per_year(sheet, row_num):
+    return sheet.cell(row_num, 9).value
+def average_aggravated_assault_per_year(sheet, row_num):
+    return sheet.cell(row_num, 11).value
+def average_burglary_per_year(sheet, row_num):
+    return sheet.cell(row_num, 15).value
+def average_larceny_per_year(sheet, row_num):
+    return sheet.cell(row_num, 17).value
+def average_vehicle_theft_per_year(sheet, row_num):
+    return sheet.cell(row_num, 19).value
+
+def second_question():
+    xlsBook = xlrd.open_workbook('./output.xls')
+    sheet = xlsBook.sheet_by_index(0)
+
+    crimes_1995 = [average_murder_per_year(sheet, 4), average_rape_per_year(sheet, 4), average_robbery_per_year(sheet, 4), average_aggravated_assault_per_year(sheet, 4), average_burglary_per_year(sheet, 4), average_larceny_per_year(sheet, 4), average_vehicle_theft_per_year(sheet, 4)]
+    crimes_2013 = [average_murder_per_year(sheet, 23), average_rape_per_year(sheet, 23), average_robbery_per_year(sheet, 23), average_aggravated_assault_per_year(sheet, 23), average_burglary_per_year(sheet, 23), average_larceny_per_year(sheet, 23), average_vehicle_theft_per_year(sheet, 23)]
+
+    print(crimes_1995)
+    print(crimes_2013)
+
+    fig = plt.figure()
+    x = np.array([0,1,2,3,4,5,6])
+    my_xticks= ["Murder", "Rape", "Robbery", "Assault", "Burglary", "Larceny", "GTA"]
+    plt.xticks(x, my_xticks)    
+    _1995 = plt.bar(my_xticks, crimes_1995, color='r')
+    _2013 = plt.bar(my_xticks, crimes_2013, color='b')
+    plt.legend((_1995[0],_2013[0]),("1995", "2013"))
+    plt.title("Crime Rates")
+    plt.xlabel("Crime Type")
+    plt.ylabel("Rate")
+
+    fig.savefig('test.png')
+    
+second_question()

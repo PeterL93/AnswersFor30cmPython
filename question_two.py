4f3a3ded9dbbac84f944330fcde4d8a335d3ebaa
J-Egeberg <jregeberg@gmail.com>
2018-02-25 23:36:16 +0100

    fuck this shit

--- question_two.py
+++ question_two.py
@@ -27,24 +27,23 @@ def average_vehicle_theft_per_year(sheet, row_num):
 
 sheet = get_years_sheet()
 
-crimes_1995 = np.array(
-    [average_murder_per_year(sheet, 4), average_rape_per_year(sheet, 4), average_robbery_per_year(sheet, 4),
+crimes_1995 = (average_murder_per_year(sheet, 4), average_rape_per_year(sheet, 4), average_robbery_per_year(sheet, 4),
      average_aggravated_assault_per_year(sheet, 4), average_burglary_per_year(sheet, 4),
-     average_larceny_per_year(sheet, 4), average_vehicle_theft_per_year(sheet, 4)])
-crimes_2013 = np.array(
-    [average_murder_per_year(sheet, 23), average_rape_per_year(sheet, 23), average_robbery_per_year(sheet, 23),
+     average_larceny_per_year(sheet, 4), average_vehicle_theft_per_year(sheet, 4))
+crimes_2013 = (average_murder_per_year(sheet, 23), average_rape_per_year(sheet, 23), average_robbery_per_year(sheet, 23),
      average_aggravated_assault_per_year(sheet, 23), average_burglary_per_year(sheet, 23),
-     average_larceny_per_year(sheet, 23), average_vehicle_theft_per_year(sheet, 23)])
+     average_larceny_per_year(sheet, 23), average_vehicle_theft_per_year(sheet, 23))
 
 print(crimes_1995)
 print(crimes_2013)
 
 fig = plt.figure()
-x = np.array([0, 1, 2, 3, 4, 5, 6])
-my_xticks = ["Murder", "Rape", "Robbery", "Assault", "Burglary", "Larceny", "GTA"]
-plt.xticks(x, my_xticks)
-_1995 = plt.bar(my_xticks, crimes_1995, color='r')
-_2013 = plt.bar(my_xticks, crimes_2013, color='b')
+ind = np.arange(7)
+
+_1995 = plt.bar(ind, crimes_1995, color='r')
+_2013 = plt.bar(ind, crimes_2013, color='b')
+
+plt.xticks(ind, ("Murder", "Rape", "Robbery", "Assault", "Burglary", "Larceny", "GTA"))
 plt.legend((_1995[0], _2013[0]), ("1995", "2013"))
 plt.title("Crime Rates")
 plt.xlabel("Crime Type")

e82955a2b1ddfbc83a50db9d879eb013c2906772
TheLegendOfProgramming <cph-mb402@cphbusiness.dk>
2018-02-26 00:47:16 +0100

    Cannot delete useless data from merged_cells. I quit for today.

--- question_four.py
+++ question_four.py
@@ -1,14 +1,33 @@
 from file_handler import get_locations_sheet
 import pprint
+import numpy as np
+import matplotlib.pyplot as plt
 
-def population_sum()
+def populations_list(merged_cells):
+    populations_list = list()
+    for merged_cell in merged_cells:
+        sum = 0
+        for row in range(merged_cell[0], merged_cell[1]):
+            sum += sheet.cell(row, 2).value
+        populations_list.append(sum)
+    return populations_list
 
 sheet = get_locations_sheet()
 pp = pprint.PrettyPrinter(indent=4)
 
 merged_cells = sorted(sheet.merged_cells, key=lambda merged_cell: merged_cell[0])
 
-pp.pprint(merged_cells)
-print(sheet.cell(4, 0))
+# print(sheet.cell(4, 0))
+print(len(merged_cells) - 11)
+print(merged_cells[len(merged_cells) - 11])
 
-# for merged_cell in merged_cells:
+for x in range(len(merged_cells) - 12, len(merged_cells) - 2):
+    del merged_cells[x]
+
+# pp.pprint(merged_cells)
+
+ind = np.arange(len(merged_cells))
+
+# fig, ax = plt.subplots()
+
+# pp.pprint(populations_list(merged_cells))
